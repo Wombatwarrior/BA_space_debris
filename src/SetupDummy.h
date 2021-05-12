eb64bc61f535ebbec49980ed89b58b0cf77ca203
BoesingO <oliver.boesing@tum.de>
2021-05-12 18:13:47 +0200

    Initial project setup with some dummy testing fpr gtest

---
+++ src/SetupDummy.h
@@ -0,0 +1,10 @@
+//
+// Created by ubuntu on 12.05.21.
+//
+
+#pragma once
+
+namespace SetupDummy {
+    int getOne();
+    int getTwo();
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Oliver on 12.05.21.
+//
+
+#include <iostream>
+#include "SetupDummy.h"
+
+int main(){
+    std::cout << SetupDummy::getOne() << "+" << SetupDummy::getOne() << "=" << SetupDummy::getTwo() << std::endl;
+}
