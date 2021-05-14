cae4e456f0e2b8578ab2f3f325e7d951ec120e11
BoesingO <oliver.boesing@tum.de>
2021-05-14 15:31:12 +0200

    -fixed acceleration typos -implemented acceleration accumulation

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,19 @@
 // Created by Oliver on 12.05.21.
 //
 
+#include "debris/DebrisContainer.h"
+#include "physics/Acceleration.h"
+#include "physics/Integrator.h"
 
 int main(){
-
+    Debris::DebrisContainer c;
+    Debris::Debris d;
+    d.setPosition({1,2,3});
+    c.addDebris(d);
+    std::array<bool,8> config{true,false,false,false,false,false,false,false};
+    Acceleration::AccelerationAccumulator ac(config, c);
+    ac.applyComponents();
+    Integrator i(c);
+    i.setDeltaT(0.5);
+    i.integrate();
 }
---
+++ src/physics/Acceleration.cpp
@@ -0,0 +1,106 @@
+//
+// Created by ubuntu on 12.05.21.
+//
+
+#include "Acceleration.h"
+#include <iostream>
+
+namespace Acceleration{
+    AccelerationAccumulator::~AccelerationAccumulator(){
+
+    }
+
+    void AccelerationAccumulator::applyComponents(){
+        std::array<double,3> new_acc_total{0,0,0};
+        std::array<double,3> new_acc_component{0,0,0};
+
+        for (auto &d : debris.getDebrisVector()) {
+            if (config[KEP]) {
+                KepComponent::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[J2]) {
+                J2Component::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[C22]) {
+                C22Component::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[S22]) {
+                S22Component::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[SOL]) {
+                SolComponent::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[LUN]) {
+                LunComponent::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[SRP]) {
+                SRPComponent::apply(d, new_acc_component, new_acc_total);
+            }
+            if (config[DRAG]) {
+                DragComponent::apply(d, new_acc_component, new_acc_total);
+            }
+            d.setAccT0(new_acc_total);
+        }
+    }
+
+    namespace KepComponent {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_kep, std::array<double,3> &acc) {
+            acc_kep = d.getPosition();
+            double divisor = acc_kep[0] * acc_kep[0] + acc_kep[1] * acc_kep[1] + acc_kep[2] * acc_kep[2];
+            divisor = divisor * divisor * divisor;
+            divisor = std::sqrt(divisor);
+            acc_kep[0] = Physics::GM_EARTH * acc_kep[0];
+            acc_kep[1] = Physics::GM_EARTH * acc_kep[1];
+            acc_kep[2] = Physics::GM_EARTH * acc_kep[2];
+            acc_kep[0] = acc_kep[0] / divisor;
+            acc_kep[1] = acc_kep[1] / divisor;
+            acc_kep[2] = acc_kep[2] / divisor;
+            acc[0] = acc[0] + acc_kep[0];
+            acc[1] = acc[1] + acc_kep[1];
+            acc[2] = acc[2] + acc_kep[2];
+        }
+    }
+
+
+    namespace J2Component {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_j2, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace C22Component {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_c22, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace S22Component {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_s22, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace SolComponent {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_sol, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace LunComponent {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_lun, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace SRPComponent {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_srp, std::array<double,3> &acc){
+
+        }
+    }
+
+    namespace DragComponent  {
+        void apply(Debris::Debris &d, std::array<double,3> &acc_drag, std::array<double,3> &acc){
+
+        }
+    }
+}
