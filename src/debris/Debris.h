eac58619dd6602b09d35d071612701cf0d043a8f
BoesingO <oliver.boesing@tum.de>
2021-05-17 01:56:33 +0200

    - docu for Debris::DebrisContainer - fixed docu references to variables

--- src/debris/Debris.h
+++ src/debris/Debris.h
@@ -20,23 +20,23 @@ namespace Debris {
         /**
          * @brief Default constructor
          *
-         * Initializes all member variables with zeros
+         * Creates a new Debris object and initializes all member variables with zero values
          */
         Debris();
 
         /**
          * @brief Copy constructor
          *
-         * Creates a new debris object and copies all member variables
+         * Creates a new Debris object and copies all member variables
          *
-         * @param other Reference to the debris object to copy
+         * @param other Reference to the Debris object to copy
          */
         Debris(const Debris &other);
 
         /**
          * @brief Default destructor
          *
-         * Destroys the debris object
+         * Destroys the Debris object
          */
         virtual ~Debris();
 
@@ -45,7 +45,7 @@ namespace Debris {
          *
          * Creates a std::string representation of the state of the debris
          *
-         * @return The string "Debris: X: ::position v: ::velocity a0: ::acc_t0 a1: ::acc_t1"
+         * @return The string "Debris: X:#position v:#velocity a0:#acc_t0 a1:#acc_t1"
          */
         std::string toString();
     private:
@@ -55,58 +55,58 @@ namespace Debris {
         std::array<double,3> acc_t1;/**< 3D vector representation of the debris acceleration at the current time step*/
     public:
         /**
-         * @brief Getter function for ::position vector
+         * @brief Getter function for #position vector
          *
-         * @return 3D vector representation of the debris position
+         * @return 3D vector representation of the debris #position
          */
         std::array<double, 3> &getPosition() ;
 
         /**
-         * @brief Getter function for ::position vector
+         * @brief Getter function for #position vector
          *
-         * @param position 3D vector representation of the debris position
+         * @param position 3D vector representation of the debris #position
          */
         void setPosition( std::array<double, 3> &position);
 
         /**
-         * @brief Getter function for ::velocity vector
+         * @brief Getter function for #velocity vector
          *
-         * @return 3D vector representation of the debris velocity
+         * @return 3D vector representation of the debris #velocity
          */
         std::array<double, 3> &getVelocity() ;
 
         /**
-         * @brief Getter function for ::velocity vector
+         * @brief Getter function for #velocity vector
          *
-         * @param velocity 3D vector representation of the debris velocity
+         * @param velocity 3D vector representation of the debris #velocity
          */
         void setVelocity( std::array<double, 3> &velocity);
 
         /**
-         * @brief Getter function for ::acc_t0 vector
+         * @brief Getter function for #acc_t0 vector
          *
-         * @return 3D vector representation of the debris acc_t0
+         * @return 3D vector representation of the debris #acc_t0
          */
         std::array<double, 3> &getAccT0() ;
 
         /**
-         * @brief Getter function for ::acc_t0 vector
+         * @brief Getter function for #acc_t0 vector
          *
-         * @param accT0 3D vector representation of the debris acc_t0
+         * @param accT0 3D vector representation of the debris #acc_t0
          */
         void setAccT0( std::array<double, 3> &accT0);
 
         /**
-         * @brief Getter function for ::acc_t1 vector
+         * @brief Getter function for #acc_t1 vector
          *
-         * @return 3D vector representation of the debris acc_t1
+         * @return 3D vector representation of the debris #acc_t1
          */
         std::array<double, 3> &getAccT1() ;
 
         /**
-         * @brief Getter function for ::position acc_t1
+         * @brief Getter function for #acc_t1 vector
          *
-         * @param accT1 3D vector representation of the debris acc_t1
+         * @param accT1 3D vector representation of the debris #acc_t1
          */
         void setAccT1( std::array<double, 3> &accT1);
     };
--- src/debris/DebrisContainer.h
+++ src/debris/DebrisContainer.h
@@ -6,19 +6,66 @@
 #include "Debris.h"
 #include <vector>
 namespace Debris {
+    /**
+     * @class DebrisContainer
+     * @brief Holds a vector of Debris objects
+     */
     class DebrisContainer {
     public:
+        /**
+         * @brief Default constructor
+         *
+         * Creates a nwe DebrisContainer object and initializes the #debris_vector empty
+         */
         DebrisContainer();
+
+        /**
+         * @brief Default destructor
+         *
+         * Destroy DebrisContainer object
+         */
         virtual ~DebrisContainer();
 
+        /**
+         * @brief Adds a Debris object to the #debris_vector
+         *
+         * Adds a copy of the given Debris object to the end of the #debris_vector
+         *
+         * @param debris Reference to the Debris object to add
+         */
         void addDebris(Debris &debris);
+
+        /**
+         * @brief Clear the #debris_vector
+         *
+         * Remove all Debris objects from the #debris_vector by calling it's clear() function
+         *
+         */
         void cleanDebrisVector();
+
+        /**
+         * @brief Move the acceleration values of the Debris objects by one time step
+         *
+         * Sets the new Debris::acc_t0 value to be the old Debris::acc_t1 value.
+         * Sets the new Debris::acc_t1 value to be a zero vector
+         */
         void shiftAcceleration();
     private:
-        std::vector<Debris> debris_vector;
-    public:
-         std::vector<Debris> &getDebrisVector() ;
 
+        std::vector<Debris> debris_vector;/**< std::vector holding the Debris objects this DebrisContainer objects holds */
+    public:
+        /**
+         * @brief Getter function for #debris_vector vector
+         *
+         * @return Reference to the #debris_vector
+         */
+        std::vector<Debris> &getDebrisVector() ;
+
+        /**
+         * @brief Setter function for #debris_vector
+         *
+         * @param debrisVector Reference to the new #debris_vector
+         */
         void setDebrisVector( std::vector<Debris> &debrisVector);
     };
 }
