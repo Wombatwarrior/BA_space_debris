4812ccf10322dc81458cd6788cb914cbb7bd64b9
BoesingO <oliver.boesing@tum.de>
2021-05-18 22:44:08 +0200

    -unit tests for J2

--- test/physics_test/Acceleration_test.cpp
+++ test/physics_test/Acceleration_test.cpp
@@ -71,7 +71,7 @@ TEST_F(KepComponentTests, CalculationEquivalenceTest){
     for (int i = 0; i < debris->getDebrisVector().size(); ++i){
         EXPECT_NEAR(accelerations_1[i][0], accelerations_2[i][0], abs_err);
         EXPECT_NEAR(accelerations_1[i][1], accelerations_2[i][1], abs_err);
-        ASSERT_NEAR(accelerations_1[i][2], accelerations_2[i][2], abs_err);
+        EXPECT_NEAR(accelerations_1[i][2], accelerations_2[i][2], abs_err);
     }
 }
 
@@ -90,10 +90,46 @@ TEST_F(KepComponentTests, EquilavelnceWIthPreCalculatedTest){
     for (int i = 0; i < num_debris; ++i){
         EXPECT_NEAR(accelerations[i][0], pre_calculated[i][0], abs_err);
         EXPECT_NEAR(accelerations[i][1], pre_calculated[i][1], abs_err);
-        ASSERT_NEAR(accelerations[i][2], pre_calculated[i][2], abs_err);
+        EXPECT_NEAR(accelerations[i][2], pre_calculated[i][2], abs_err);
     }
 }
 
-TEST_F(J2ComponentTests, test1){
+TEST_F(J2ComponentTests, CalculationEquivalenceTest){
+    const int num_debris = 9;
+    std::array<std::array<double,3>, num_debris> accelerations_1;
+    std::array<std::array<double,3>, num_debris> accelerations_2;
+    std::array<double,3> acc_total_dummy;
 
+    // calculate the acceleration for all particles using two different functions
+    for(int i = 0; i < debris->getDebrisVector().size(); ++i){
+        Acceleration::J2Component::apply(debris->getDebrisVector()[i],accelerations_1[i],acc_total_dummy);
+        calcJ2(debris->getDebrisVector()[i],accelerations_2[i]);
+    }
+
+    // 10e-20 fails, but e-19 passes
+    double abs_err = 10e-10;
+    for (int i = 0; i < debris->getDebrisVector().size(); ++i){
+        EXPECT_NEAR(accelerations_1[i][0], accelerations_2[i][0], abs_err);
+        EXPECT_NEAR(accelerations_1[i][1], accelerations_2[i][1], abs_err);
+        EXPECT_NEAR(accelerations_1[i][2], accelerations_2[i][2], abs_err);
+    }
+}
+
+TEST_F(J2ComponentTests, EquilavelnceWIthPreCalculatedTest){
+    const int num_debris = 9;
+    std::array<std::array<double,3>, num_debris> accelerations;
+    std::array<double,3> acc_total_dummy;
+
+    // calculate the acceleration for all particles using two different functions
+    for(int i = 0; i < debris->getDebrisVector().size(); ++i){
+        Acceleration::J2Component::apply(debris->getDebrisVector()[i], accelerations[i], acc_total_dummy);
+    }
+
+    // 10e-22 fails, but e-21 passes
+    double abs_err = 10e-21;
+    for (int i = 0; i < num_debris; ++i){
+        EXPECT_NEAR(accelerations[i][0], pre_calculated[i][0], abs_err);
+        EXPECT_NEAR(accelerations[i][1], pre_calculated[i][1], abs_err);
+        EXPECT_NEAR(accelerations[i][2], pre_calculated[i][2], abs_err);
+    }
 }
--- test/physics_test/Acceleration_test.h
+++ test/physics_test/Acceleration_test.h
@@ -64,15 +64,88 @@ protected:
 
     void calcKep(Debris::Debris &d, std::array<double,3> &acc_kep){
         double gme=3.986004407799724e+5;
-        acc_kep[0] = -(gme*d.getPosition()[0])/std::pow((std::pow(d.getPosition()[0],2)+std::pow(d.getPosition()[1],2)+std::pow(d.getPosition()[2],2)),1.5);
-        acc_kep[1] = -(gme*d.getPosition()[1])/std::pow((std::pow(d.getPosition()[0],2)+std::pow(d.getPosition()[1],2)+std::pow(d.getPosition()[2],2)),1.5);
-        acc_kep[2] = -(gme*d.getPosition()[2])/std::pow((std::pow(d.getPosition()[0],2)+std::pow(d.getPosition()[1],2)+std::pow(d.getPosition()[2],2)),1.5);
+        double x = d.getPosition()[0];
+        double y = d.getPosition()[1];
+        double z = d.getPosition()[2];
+        double d1 = std::pow((x*x+y*y+z*z), 1.5);
+        acc_kep[0] =-((gme*x)/d1);
+        acc_kep[1] =-((gme*y)/d1);
+        acc_kep[2] =-((gme*z)/d1);
     }
 };
 
 class J2ComponentTests : public ::testing::Test {
 protected:
-    virtual void SetUp() {
+    std::shared_ptr<Debris::DebrisContainer> debris;
+    std::array<std::array<double,3>,9> pre_calculated;
 
+    virtual void SetUp() {
+        debris = std::make_shared<Debris::DebrisContainer>();
+        Debris::Debris d;
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                std::array<double, 3> pos{0, 0, 0};
+                pos[j] = (i+2)*3.5e3;
+                d.setPosition(pos);
+                debris->addDebris(d);
+            }
+        }
+        // calculated with wolfram alpha
+        pre_calculated[0] = {-0.000010967387600710096587782776206071373077501857198479655576780,
+                             0,
+                             0
+        };
+        pre_calculated[1] = {0,
+                             -0.000010967387600710096587782776206071373077501857198479655576780,
+                             0
+        };
+        pre_calculated[2] = {0,
+                             0,
+                             0.0000219347752014201931755655524121427461550037143969593111535607
+        };
+        pre_calculated[3] = {-2.166397550757549943265733571569653941234934755255240607759e-6,
+                             0,
+                             0
+        };
+        pre_calculated[4] = {0,
+                             -2.166397550757549943265733571569653941234934755255240607759e-6,
+                             0
+        };
+        pre_calculated[5] = {0,
+                             0,
+                             4.3327951015150998865314671431393078824698695105104812155181e-6
+        };
+        pre_calculated[6] = {-6.854617250443810367364235128794608173438660749049784735487e-7,
+                             0,
+                             0
+        };
+        pre_calculated[7] = {0,
+                             -6.854617250443810367364235128794608173438660749049784735487e-7,
+                             0
+        };
+        pre_calculated[8] = {0,
+                             0,
+                             1.3709234500887620734728470257589216346877321498099569470975e-6
+        };
+
+    }
+
+    void calcJ2(Debris::Debris &d, std::array<double,3> &acc_j2){
+        double gme = 3.986004407799724e+5;
+        double re = 6378.1363;
+        double c20 = -4.84165371736e-4;
+
+        double x = d.getPosition()[0];
+        double y = d.getPosition()[1];
+        double z = d.getPosition()[2];
+        double n1 = gme*re*re*std::sqrt(5)*c20;
+        double d1 = 2*std::sqrt(x*x+y*y+z*z);
+        double d2 = (x*x+y*y+z*z)*(x*x+y*y+z*z);
+        double n3 = 15*(z*z);
+        double d3 = (x*x+y*y+z*z)*(x*x+y*y+z*z)*(x*x+y*y+z*z);
+        acc_j2[0] = ((n1*x)/d1)*((3/d2)-(n3/d3));
+        acc_j2[1] = ((n1*y)/d1)*((3/d2)-(n3/d3));
+        acc_j2[2] = ((n1*z)/d1)*((9/d2)-(n3/d3));
     }
 };
