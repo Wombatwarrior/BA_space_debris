44d57015e8f23429f07f13b709a020f430e91f87
BoesingO <oliver.boesing@tum.de>
2021-06-30 14:00:13 +0200

    SRP Test added

--- test/physics_test/Integrator_test.cpp
+++ test/physics_test/Integrator_test.cpp
@@ -292,16 +292,48 @@ TEST_F(CompareWithHeyokaTests, compareSol)
 // compare calculated values of SRPComponent
 TEST_F(CompareWithHeyokaTests, compareSRP)
 {
+    // time step in seconds
+    double delta_t = 0.1;
     // set some test debris values
     std::vector<Debris::Debris> ds;
     Debris::Debris d;
     for (int i = 0; i < 3; ++i){
         d.setPosition({3500.*(i+2),0,0});
         d.setVelocity({10.,0,0});
-        d.setAom(0.5);
         ds.push_back(d);
     }
-
+    // loop over the debris data and compare calculations
+    for (auto d : ds){
+        // setup integrators
+        i_components[Acceleration::SRP]->getDebris().cleanDebrisVector();
+        i_components[Acceleration::SRP]->getDebris().addDebris(d);
+        ta_components[Acceleration::SRP]->get_state_data()[0] = d.getPosition()[0];
+        ta_components[Acceleration::SRP]->get_state_data()[1] = d.getPosition()[1];
+        ta_components[Acceleration::SRP]->get_state_data()[2] = d.getPosition()[2];
+        ta_components[Acceleration::SRP]->get_state_data()[3] = d.getVelocity()[0];
+        ta_components[Acceleration::SRP]->get_state_data()[4] = d.getVelocity()[1];
+        ta_components[Acceleration::SRP]->get_state_data()[5] = d.getVelocity()[2];
+        // reset time values
+        i_components[Acceleration::SRP]->setDeltaT(delta_t);
+        i_components[Acceleration::SRP]->getAccumulator().setT(0.);
+        ta_components[Acceleration::SRP]->set_time(0.);
+        // integrate time step
+        i_components[Acceleration::SRP]->integrate();
+        ta_components[Acceleration::SRP]->step(delta_t);
+        // compare result
+        std::array<double,3> pos_i = i_components[Acceleration::SRP]->getDebris().getDebrisVector()[0].getPosition();
+        std::array<double,3> vel_i = i_components[Acceleration::SRP]->getDebris().getDebrisVector()[0].getVelocity();
+        std::array<double,3> pos_ta{ta_components[Acceleration::SRP]->get_state()[0],
+                                    ta_components[Acceleration::SRP]->get_state()[1],
+                                    ta_components[Acceleration::SRP]->get_state()[2]};
+        std::array<double,3> vel_ta{ta_components[Acceleration::SRP]->get_state()[3],
+                                    ta_components[Acceleration::SRP]->get_state()[4],
+                                    ta_components[Acceleration::SRP]->get_state()[5]};
+        IOUtils::to_ostream(pos_i, std::cout, ",", {"position integrator[","]\n"});
+        IOUtils::to_ostream(pos_ta, std::cout, ",", {"position heyoka[","]\n"});
+        IOUtils::to_ostream(vel_i, std::cout, ",", {"velocity integrator[","]\n"});
+        IOUtils::to_ostream(vel_ta, std::cout, ",", {"velocity heyoka[","]\n"});
+    }
 }
 // compare calculated values of DragComponent
 TEST_F(CompareWithHeyokaTests, compareDrag)
