1e175189bf4cbb8c23f40a8ccdba1a12e77d4741
BoesingO <oliver.boesing@tum.de>
2021-06-30 13:53:10 +0200

    J2 Test added

--- test/physics_test/Integrator_test.cpp
+++ test/physics_test/Integrator_test.cpp
@@ -17,7 +17,7 @@ TEST_F(CompareWithHeyokaTests, showTaylorIntegrator)
 // compare calculated values of KepComponent
 TEST_F(CompareWithHeyokaTests, compareKep) {
     // time step in seconds
-    double delta_t = 1.0;
+    double delta_t = 0.1;
     // set some test debris values and add them to the Integrators
     std::vector<Debris::Debris> ds;
     Debris::Debris d;
@@ -41,11 +41,9 @@ TEST_F(CompareWithHeyokaTests, compareKep) {
         i_components[Acceleration::KEP]->setDeltaT(delta_t);
         i_components[Acceleration::KEP]->getAccumulator().setT(0.);
         ta_components[Acceleration::KEP]->set_time(0.);
-        std::cout << *ta_components[Acceleration::KEP] << std::endl;
         // integrate time step
         i_components[Acceleration::KEP]->integrate();
-        ta_components[Acceleration::KEP]->propagate_until(delta_t);
-        std::cout << *ta_components[Acceleration::KEP] << std::endl;
+        ta_components[Acceleration::KEP]->step(delta_t);
         // compare result
         std::array<double,3> pos_i = i_components[Acceleration::KEP]->getDebris().getDebrisVector()[0].getPosition();
         std::array<double,3> vel_i = i_components[Acceleration::KEP]->getDebris().getDebrisVector()[0].getVelocity();
@@ -64,6 +62,8 @@ TEST_F(CompareWithHeyokaTests, compareKep) {
 // compare calculated values of J2Component
 TEST_F(CompareWithHeyokaTests, compareJ2)
 {
+    // time step in seconds
+    double delta_t = 0.1;
     // set some test debris values
     std::vector<Debris::Debris> ds;
     Debris::Debris d;
@@ -72,7 +72,38 @@ TEST_F(CompareWithHeyokaTests, compareJ2)
         d.setVelocity({10.,0,0});
         ds.push_back(d);
     }
-
+    // loop over the debris data and compare calculations
+    for (auto d : ds){
+        // setup integrators
+        i_components[Acceleration::J2]->getDebris().cleanDebrisVector();
+        i_components[Acceleration::J2]->getDebris().addDebris(d);
+        ta_components[Acceleration::J2]->get_state_data()[0] = d.getPosition()[0];
+        ta_components[Acceleration::J2]->get_state_data()[1] = d.getPosition()[1];
+        ta_components[Acceleration::J2]->get_state_data()[2] = d.getPosition()[2];
+        ta_components[Acceleration::J2]->get_state_data()[3] = d.getVelocity()[0];
+        ta_components[Acceleration::J2]->get_state_data()[4] = d.getVelocity()[1];
+        ta_components[Acceleration::J2]->get_state_data()[5] = d.getVelocity()[2];
+        // reset time values
+        i_components[Acceleration::J2]->setDeltaT(delta_t);
+        i_components[Acceleration::J2]->getAccumulator().setT(0.);
+        ta_components[Acceleration::J2]->set_time(0.);
+        // integrate time step
+        i_components[Acceleration::J2]->integrate();
+        ta_components[Acceleration::J2]->step(delta_t);
+        // compare result
+        std::array<double,3> pos_i = i_components[Acceleration::J2]->getDebris().getDebrisVector()[0].getPosition();
+        std::array<double,3> vel_i = i_components[Acceleration::J2]->getDebris().getDebrisVector()[0].getVelocity();
+        std::array<double,3> pos_ta{ta_components[Acceleration::J2]->get_state()[0],
+                                    ta_components[Acceleration::J2]->get_state()[1],
+                                    ta_components[Acceleration::J2]->get_state()[2]};
+        std::array<double,3> vel_ta{ta_components[Acceleration::J2]->get_state()[3],
+                                    ta_components[Acceleration::J2]->get_state()[4],
+                                    ta_components[Acceleration::J2]->get_state()[5]};
+        IOUtils::to_ostream(pos_i, std::cout, ",", {"position integrator[","]\n"});
+        IOUtils::to_ostream(pos_ta, std::cout, ",", {"position heyoka[","]\n"});
+        IOUtils::to_ostream(vel_i, std::cout, ",", {"velocity integrator[","]\n"});
+        IOUtils::to_ostream(vel_ta, std::cout, ",", {"velocity heyoka[","]\n"});
+    }
 }
 // compare calculated values of C22Component
 TEST_F(CompareWithHeyokaTests, compareC22)
