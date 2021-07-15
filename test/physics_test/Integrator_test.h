b1d7a3de04cb9e53fb998fb9fb0a15778c2bacc5
BoesingO <oliver.boesing@tum.de>
2021-07-15 13:48:57 +0200

    Added total velocity to output for ta_split

--- test/physics_test/Integrator_test.h
+++ test/physics_test/Integrator_test.h
@@ -37,7 +37,7 @@ protected:
     // delta t
     inline static double delta_t = .1;
     inline static constexpr double start_t = 0.;
-    inline static constexpr double end_t = 1000;
+    inline static constexpr double end_t = 10;
 
     //heyoka variables
     inline static heyoka::taylor_adaptive<double>* ta_total;
@@ -289,7 +289,7 @@ protected:
         // prepare output file
         ta_split_out = new std::ofstream(std::to_string(time_stamp) + "/heyoka_split.csv");
         *ta_split_out << std::setprecision(std::numeric_limits<double>::digits10 + 1);
-        *ta_split_out << "index,time,position x,position y,position z,position norm,vel_kep x,vel_kep y,vel_kep z,vel_kep norm,vel_j2 x,vel_j2 y,vel_j2 z,vel_j2 norm,vel_c22 x,vel_c22 y,vel_c22 z,vel_c22 norm,vel_s22 x,vel_s22 y,vel_s22 z,vel_s22 norm,vel_sol x,vel_sol y,vel_sol z,vel_sol norm,vel_lun x,vel_lun y,vel_lun z,vel_lun norm,vel_srp x,vel_srp y,vel_srp z,vel_srp norm,vel_drag x,vel_drag y,vel_drag z,vel_drag norm,vel_total x,vel_total y,vel_total z,vel_total norm" << std::endl;
+        *ta_split_out << "index,time,position x,position y,position z,position norm,vel_init x,vel_init y,vel_init z,vel_init norm,vel_kep x,vel_kep y,vel_kep z,vel_kep norm,vel_j2 x,vel_j2 y,vel_j2 z,vel_j2 norm,vel_c22 x,vel_c22 y,vel_c22 z,vel_c22 norm,vel_s22 x,vel_s22 y,vel_s22 z,vel_s22 norm,vel_sol x,vel_sol y,vel_sol z,vel_sol norm,vel_lun x,vel_lun y,vel_lun z,vel_lun norm,vel_srp x,vel_srp y,vel_srp z,vel_srp norm,vel_drag x,vel_drag y,vel_drag z,vel_drag norm,vel_total x,vel_total y,vel_total z,vel_total norm" << std::endl;
 
         ta_components[Acceleration::KEP] = new heyoka::taylor_adaptive<double> {
             { heyoka::prime(pos[0]) = dXdt, heyoka::prime(pos[1]) = dYdt, heyoka::prime(pos[2]) = dZdt, heyoka::prime(vel[0]) = fKepX, heyoka::prime(vel[1]) = fKepY, heyoka::prime(vel[2]) = fKepZ },
