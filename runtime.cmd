3b4167205cb3586f7bb567e36e35328e664a506b
BoesingO <oliver.boesing@tum.de>
2021-09-08 12:55:43 +0200

    slurm jobs

--- runtime.cmd
+++ runtime.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J BA_profile
+#SBATCH -J BA_runtime_analysis
 #SBATCH -o ./../../output/%x.%j.%N.out
 #SBATCH -D ./build
 #SBATCH --clusters=serial
@@ -11,13 +11,8 @@
 #SBATCH --export=NONE
 #SBATCH --time=02:00:00
 module load slurm_setup
-module load amplifier_xe
 module load llvm/10.0.0
 module unload intel-mpi/2019-intel
 module unload intel
 module switch gcc/10.2.0
-source ${AMPLIFIER_XE_BASE}/apsvars.sh
-rm -rf ../result
-mkdir ../result
-:: aps --result-dir=./../result ./debris_sim ../test_files/test_input.txt -c
-amplxe-cl -collect hotspots -result-dir ./../result -quiet ./debris_sim ../test_files/test_input.txt -c
+./debris_sim ../test_files/test_input.txt -c
