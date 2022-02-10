1999570fdb07a4aae876145b3ee6c5a96ba4f7a9
FG-TUM <f.gratl@tum.de>
2022-02-10 13:51:22 +0100

    formatting

--- src/satellitePropagator/physics/AccelerationComponents/DragComponent.h
+++ src/satellitePropagator/physics/AccelerationComponents/DragComponent.h
@@ -10,17 +10,17 @@ namespace Acceleration::DragComponent {
 namespace {
 }
 /**
-     * @brief Calculates acceleration due to the friction between the object and
-     * the earths atmosphere
-     *
-     * @note Model source: https://farside.ph.utexas.edu/teaching/celestial/Celestial/node94.html
-     *
-     * @param d Reference to the Debris::Debris object to apply the acceleration to
-     * @param acc_drag Reference to an 3D vector to write the result for this
-     * Acceleration::AccelerationComponent.
-     * @param acc_total Reference to an 3D vector to accumulate the accelerations
-     * for all applied Acceleration::AccelerationComponent.
-     */
+ * @brief Calculates acceleration due to the friction between the object and
+ * the earths atmosphere
+ *
+ * @note Model source: https://farside.ph.utexas.edu/teaching/celestial/Celestial/node94.html
+ *
+ * @param d Reference to the Debris::Debris object to apply the acceleration to
+ * @param acc_drag Reference to an 3D vector to write the result for this
+ * Acceleration::AccelerationComponent.
+ * @param acc_total Reference to an 3D vector to accumulate the accelerations
+ * for all applied Acceleration::AccelerationComponent.
+ */
 template <class D>
 auto apply(const D& d);
 
@@ -30,11 +30,11 @@ template <class D>
 auto apply(const D& d)
 {
     // aliases
-    const auto &vObj = d.getVelocity();
-    const auto &pObj = d.getPosition();
+    const auto& vObj = d.getVelocity();
+    const auto& pObj = d.getPosition();
 
     // Thesis Equation 2.31
-    const std::array<double, 3> vRel{
+    const std::array<double, 3> vRel {
         vObj[0] + Physics::ROT_ATMOSPHERE * pObj[1],
         vObj[1] - Physics::ROT_ATMOSPHERE * pObj[0],
         vObj[2],
@@ -46,7 +46,7 @@ auto apply(const D& d)
     const double p = Physics::P_GROUND * std::exp(-(d.getHeight() - Physics::R_EARTH) * H_ATMOSPHERE_inv);
     // Equation 10.130 / Thesis Equation 2.30
     const auto factor = -0.5 * p * d.getBcInv() * vNorm;
-    return std::array<double, 3>{
+    return std::array<double, 3> {
         vRel[0] * factor,
         vRel[1] * factor,
         vRel[2] * factor,
